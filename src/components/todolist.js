0ffb844d1b30bb9975bfb551b5fbb5abf91266a9
Vidheesh Kumar Nacode <vid@hedwig.local>
2021-03-29 23:17:40 +0530

    t1

---
+++ src/components/todolist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Todo from './Todo';
+
+const todolist = ({todos, setTodos, filteredTodos }) => {
+   return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map(todo => (
+          <Todo  setTodos={setTodos} todos={todos} key= {todo.id} todo = {todo} text= {todo.text} />
+        ))}
+        </ul>       
+    </div>
+    );
+};
+export default todolist;
